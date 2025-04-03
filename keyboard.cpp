6ba02761a540b5f6440f7b0aa26f57f662c085ca
ProfessorFabioRamos <61994717+ProfessorFabioRamos@users.noreply.github.com>
2025-04-03 09:00:03 -0300

    Update keyboard.cpp

--- keyboard.cpp
+++ keyboard.cpp
@@ -1,22 +1,35 @@
 #include <GL/glut.h>
+#include <iostream>
+
+using namespace std;
+
 float x_position;
+float y_position;
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1, 0, 0);
 	glBegin(GL_QUADS);
-		glVertex2f(10 + x_position, 10);
-		glVertex2f(10 + x_position, -10);
-		glVertex2f(-10 + x_position, -10);
-		glVertex2f(-10+ x_position, 10);
+		glVertex2f(10 + x_position, 10 + y_position);
+		glVertex2f(10 + x_position, -10 + y_position);
+		glVertex2f(-10 + x_position, -10 + y_position);
+		glVertex2f(-10 + x_position, 10 + y_position);
 	glEnd();
 	glFlush();
 }
 void teclado(unsigned char key, int x, int y) {
-	if (key == 100) { // letra d
+	cout << int(key)<<"\n";
+	if (key == 100) // letra d
 		x_position += 1.0f;
-	}
+	else if (key == 97) //letra a
+		x_position -= 1.0f;
+	else if (key == 119) //letra w
+		y_position += 1.0f;
+	else if (key == 115) //letra s
+		y_position -= 1.0f;
+	else if (key == 27)
+		exit(0);
 	glutPostRedisplay();
 }
 int main(int argc, char** argv)
